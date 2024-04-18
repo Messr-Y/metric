37def9d91b25f80d5116e60f09f2a269b0c97357
ReturnTmp <3055871692@qq.com>
2024-04-18 20:38:06 +0800

    :art: 格式化返回结果

--- src/main/java/org/csu/softwaremetric/service/FPService.java
+++ src/main/java/org/csu/softwaremetric/service/FPService.java
@@ -4,9 +4,13 @@ import org.csu.softwaremetric.entity.UFC;
 import org.csu.softwaremetric.entity.VAF;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+
 @Service
 public class FPService {
-    public int calUFC(UFC ufc){
+    private DecimalFormat df = new DecimalFormat("#.0000");
+
+    public int calUFC(UFC ufc) {
         int ei = ufc.getEI1() * 3 + ufc.getEI2() * 4 + ufc.getEI3() * 6;
         int eq = ufc.getEQ1() * 4 + ufc.getEQ2() * 5 + ufc.getEQ3() * 7;
         int eo = ufc.getEO1() * 3 + ufc.getEO2() * 4 + ufc.getEO3() * 6;
@@ -15,16 +19,16 @@ public class FPService {
         return ei + eq + eo + eif + eif + ilf;
     }
 
-    public double calVAF(VAF vaf){
+    public double calVAF(VAF vaf) {
         int f = vaf.getRecovery() + vaf.getCommunication() + vaf.getDistribute() + vaf.getPerformance() + vaf.getOperation()
                 + vaf.getOnline() + vaf.getMultiple() + vaf.getUpdate() + vaf.getComplex() + vaf.getInterComplex()
                 + vaf.getInstallation() + vaf.getReuse() + vaf.getFacilitation() * vaf.getMultiplePoints();
-        return 0.65 + 0.01 * f;
+        return Double.parseDouble(df.format(0.65 + 0.01 * f));
     }
 
-    public double calFP(UFC ufc, VAF vaf){
+    public double calFP(UFC ufc, VAF vaf) {
         int u = calUFC(ufc);
         double v = calVAF(vaf);
-        return u * v;
+        return Double.parseDouble(df.format(u * v));
     }
 }
