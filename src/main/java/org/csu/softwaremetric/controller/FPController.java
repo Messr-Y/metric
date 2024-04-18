10c2de414fd88657abdcc2a1255a594d9bd660f6
messer-y <3242998609@qq.com>
2024-04-18 16:43:01 +0800

    修改回来

--- src/main/java/org/csu/softwaremetric/controller/FPController.java
+++ src/main/java/org/csu/softwaremetric/controller/FPController.java
@@ -13,39 +13,21 @@ public class FPController {
     @Autowired
     private FPService fpService;
 
+    private UFC ufc;
+
     @PostMapping("/getUFC")
-    CommonResponse calUFC(@RequestParam("EI1") int EI1, @RequestParam("EI2") int EI2, @RequestParam("EI3") int EI3,
-                          @RequestParam("EO1") int EO1, @RequestParam("EO2") int EO2, @RequestParam("EO3") int EO3,
-                          @RequestParam("EQ1") int EQ1, @RequestParam("EQ2") int EQ2, @RequestParam("EQ3") int EQ3,
-                          @RequestParam("EIF1") int EIF1, @RequestParam("EIF2") int EIF2, @RequestParam("EIF3") int EIF3,
-                          @RequestParam("ILF1") int ILF1, @RequestParam("ILF2") int ILF2, @RequestParam("ILF3") int ILF3) {
-        UFC ufc = new UFC(EI1, EI2, EI3, EO1, EO2, EO3, EQ1, EQ2, EQ3, EIF1, EIF2, EIF3, ILF1, ILF2, ILF3);
+    @ResponseBody
+    CommonResponse calUFC(UFC ufc) {
         return CommonResponse.createForSuccess(fpService.calUFC(ufc));
     }
 
     @PostMapping("/getVAF")
-    CommonResponse calVAF(@RequestParam("ErecoveryI1") int recovery, @RequestParam("communication") int communication, @RequestParam("distribute") int distribute,
-                          @RequestParam("performance") int performance, @RequestParam("operation") int operation, @RequestParam("online") int online,
-                          @RequestParam("multiple") int multiple, @RequestParam("update") int update, @RequestParam("complex") int complex,
-                          @RequestParam("interComplex") int interComplex, @RequestParam("reuse") int reuse, @RequestParam("installation") int installation,
-                          @RequestParam("multiplePoints") int multiplePoints, @RequestParam("facilitation") int facilitation) {
-        VAF vaf = new VAF(recovery, communication, distribute, performance, operation, online, multiple, update, complex, interComplex, reuse, installation, multiplePoints, facilitation);
+    CommonResponse calVAF(VAF vaf) {
         return CommonResponse.createForSuccess(fpService.calVAF(vaf));
     }
 
     @PostMapping("/getFP")
-    CommonResponse calFP(@RequestParam("EI1") int EI1, @RequestParam("EI2") int EI2, @RequestParam("EI3") int EI3,
-                         @RequestParam("EO1") int EO1, @RequestParam("EO2") int EO2, @RequestParam("EO3") int EO3,
-                         @RequestParam("EQ1") int EQ1, @RequestParam("EQ2") int EQ2, @RequestParam("EQ3") int EQ3,
-                         @RequestParam("EIF1") int EIF1, @RequestParam("EIF2") int EIF2, @RequestParam("EIF3") int EIF3,
-                         @RequestParam("ILF1") int ILF1, @RequestParam("ILF2") int ILF2, @RequestParam("ILF3") int ILF3,
-                         @RequestParam("ErecoveryI1") int recovery, @RequestParam("communication") int communication, @RequestParam("distribute") int distribute,
-                         @RequestParam("performance") int performance, @RequestParam("operation") int operation, @RequestParam("online") int online,
-                         @RequestParam("multiple") int multiple, @RequestParam("update") int update, @RequestParam("complex") int complex,
-                         @RequestParam("interComplex") int interComplex, @RequestParam("reuse") int reuse, @RequestParam("installation") int installation,
-                         @RequestParam("multiplePoints") int multiplePoints, @RequestParam("facilitation") int facilitation){
-        UFC ufc = new UFC(EI1, EI2, EI3, EO1, EO2, EO3, EQ1, EQ2, EQ3, EIF1, EIF2, EIF3, ILF1, ILF2, ILF3);
-        VAF vaf = new VAF(recovery, communication, distribute, performance, operation, online, multiple, update, complex, interComplex, reuse, installation, multiplePoints, facilitation);
+    CommonResponse calFP(UFC ufc, VAF vaf){
         return CommonResponse.createForSuccess(fpService.calFP(ufc, vaf));
     }
 }
